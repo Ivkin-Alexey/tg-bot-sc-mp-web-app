5a591893d54151df15a53e0ed9d09c5cc3e03106
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-08-01 00:42:34 +0300

    feat: add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
 
 const Header = () => {
-    return (
-        <div>
+    const {user, onClose} = useTelegram();
 
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
         </div>
     );
 };
