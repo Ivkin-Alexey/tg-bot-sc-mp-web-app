f10ca096058829b225b6ed2586f1fcec4a16af00
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-08-04 05:36:06 +0300

    feat: add some styles to Card component

--- src/pages/EquipmentList/EquipmentList.jsx
+++ src/pages/EquipmentList/EquipmentList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {equipment} from "../../assets/db";
-import {Box, Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
+import {Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
 import {useParams} from "react-router-dom";
 import img from'../../assets/img/Vega 3 LMH.jpeg';
+import './EquipmentList.css';
 
 const EquipmentList = () => {
 
@@ -14,25 +15,26 @@ const EquipmentList = () => {
     return (
             list.map((el, i) => {
                 return (
-                    <Card sx={{maxWidth: 345}} key={i}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                width="auto"
-                                image={img}
-                                alt={el}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {el}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                                    species, ranging across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
+                    <Card key={i}>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height="140"
+                            image={img}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {el}
+                            </Typography>
+                            {/*<Typography variant="body2" color="text.secondary">*/}
+                            {/*    Lizards are a widespread group of squamate reptiles, with over 6,000*/}
+                            {/*    species, ranging across all continents except Antarctica*/}
+                            {/*</Typography>*/}
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Старт</Button>
+                            <Button size="small">Скачать файлы</Button>
+                        </CardActions>
                     </Card>
                 )
             })
