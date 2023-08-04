c226dea4ec772f621e830c6e4a260b030e411152
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-08-05 02:34:00 +0300

    feat: change Reagents page items direction

--- src/components/RegentsFormItem/RegentsFormItem.jsx
+++ src/components/RegentsFormItem/RegentsFormItem.jsx
@@ -8,37 +8,45 @@ const RegentsFormItem = (props) => {
 
     return (
         <Stack
-            direction="row"
+            direction="rom"
             alignItems="center"
             spacing={2}
         >
-            <TextField
-                required
-                id="outlined-required"
-                label="Название"
-                name="name"
-                value={reagent.name}
-                onChange={(e) => onChangeReagent(e, index)}
-                fullWidth
-                error={false}
-            />
-            <TextField
-                required
-                id="outlined-required"
-                label="Количество"
-                name="amount"
-                value={reagent.amount}
-                onChange={(e) => {onChangeReagent(e, index)}}
-                fullWidth
-                error={false}
-            />
+            <Stack
+                direction="column"
+                alignItems="center"
+                spacing={2}
+            >
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Название"
+                    name="name"
+                    value={reagent.name}
+                    onChange={(e) => onChangeReagent(e, index)}
+                    fullWidth
+                    error={false}
+                />
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Количество"
+                    name="amount"
+                    value={reagent.amount}
+                    onChange={(e) => {
+                        onChangeReagent(e, index)
+                    }}
+                    fullWidth
+                    error={false}
+                />
+
+            </Stack>
             {index === 0 ? null :
-            <IconButton aria-label="delete" size="large" onClick={() => deleteReagent(index)}>
-                <RemoveCircleOutlineIcon size="large" color="inherit"/>
-            </IconButton>
+                <IconButton aria-label="delete" size="large" onClick={() => deleteReagent(index)}>
+                    <RemoveCircleOutlineIcon size="large" color="inherit"/>
+                </IconButton>
             }
-        </Stack>
-    );
+        </Stack>);
 };
 
 export default RegentsFormItem;
