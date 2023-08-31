c112fc69c7708203fff49c2244aec1151b71bd86
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-08-31 22:13:29 +0300

    debug

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -26,7 +26,7 @@ const Form = (props) => {
                         'Content-Type': 'application/json',
                     },
                     body: JSON.stringify(data)
-                })
+                }).then(res => console.log(res))
                 console.log(data);
             } catch (e) {
                 console.log(e);
