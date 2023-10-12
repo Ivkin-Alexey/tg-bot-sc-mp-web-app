84aa1bb22b3e1e9edd903a357811f11f734cdb12
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-10-12 20:27:51 +0300

    feat: add redux-persist to project

--- src/index.js
+++ src/index.js
@@ -6,17 +6,43 @@ import {CssBaseline} from "@mui/material";
 import {Provider} from "react-redux";
 import rootReducer from './redux/rootReducer';
 import thunk from "redux-thunk"
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import {applyMiddleware, compose, legacy_createStore as createStore} from "redux";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import { createBrowserHistory } from 'history';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer, compose(
+        applyMiddleware(
+            thunk
+        ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+let persistor = persistStore(store)
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <PersistGate loading={null} persistor={persistor}>
     <Provider store={store}>
         <BrowserRouter><React.StrictMode><CssBaseline>
             <App/>
         </CssBaseline></React.StrictMode></BrowserRouter>
     </Provider>
+    </PersistGate>
 );
 
 
