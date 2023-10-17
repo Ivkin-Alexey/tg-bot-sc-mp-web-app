6be2cf75d07bb2189e9164c03e1e64fdc5c64414
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2023-10-17 23:08:15 +0300

    debug

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {Link, useLocation, useNavigate, useParams} from 'react-router-dom';
+import {Link, useLocation, useParams} from 'react-router-dom';
+import {useNavigate} from 'react-router';
 import {useSelector} from "react-redux";
 import Profile from "../../components/Profile/Profile";
 import {Grid, IconButton, ListItem, ListItemIcon} from "@mui/material";
