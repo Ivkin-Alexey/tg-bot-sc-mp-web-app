d0875c1180c800026a8c424dbee739bc55886b73
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-02-08 00:00:06 +0300

    fix: fixing URL for safety instruction

--- src/assets/constants/constants.js
+++ src/assets/constants/constants.js
@@ -2,7 +2,7 @@ const constants = {
     port: 443,
     serverDomain: 'scmp-bot-server.ru',
     serverDomain2: 'https://e3fa3b38-234b-4eb7-b5dc-50d25274ba64.selcdn.net',
-    safetyInstructionUrl: "https://lk.spmi.ru/docs/pub/dd07a2ef99eb67a94ddf19154da8c667/download/?&token=rl4fv9mzcxr8",
+    safetyInstructionUrl: "https://lk.spmi.ru/docs/pub/dd07a2ef99eb67a94ddf19154da8c667/default/?&",
     safetyTestUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe-HSSyKiTEhi99Yb9XvYIC7zqRUo4rHcdVdOiwZ3eXVvzTwA/alreadyresponded",
     // defaultUserChatID: 883563855,
     defaultUserChatID: 392584400,
